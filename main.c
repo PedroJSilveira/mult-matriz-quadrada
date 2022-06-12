c9e04f408655471990a199eb41d145bcfba4b7b9
PedroJSilveira <pjunhosilveira@gmail.com>
2022-06-12 10:31:57 -0300

    add main.c

---
+++ main.c
@@ -0,0 +1,33 @@
+//if you want to multiply a matrix with other dimensions, change 3 to the number you want
+
+#include <stdio.h>
+
+int main() {
+  int i /*lines*/, j /*colums*/, k, soma;
+  int mat1[3][3];
+  int mat2[3][3];
+  int mult[3][3];
+  for (i = 0; i < 3; i++)
+    for (j = 0; j < 3; j++) {
+      scanf("%d", &mat1[i][j]);
+    }
+  printf("\n");
+  for (i = 0; i < 3; i++)
+    for (j = 0; j < 3; j++) {
+      scanf("%d", &mat2[i][j]);
+    }
+  for (i = 0; i < 3; i++)
+    for (j = 0; j < 3; j++) {
+      soma = 0;
+      for (k = 0; k < 3; k++)
+        soma = soma + mat1[i][k] * mat2[k][j];
+      mult[i][j] = soma;
+    }
+  for (i = 0; i < 3; i++) {
+    printf("\n");
+    for (j = 0; j < 3; j++)
+      printf("%d ", mult[i][j]);
+  }
+
+  return 0;
+}
