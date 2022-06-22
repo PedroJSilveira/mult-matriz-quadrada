2262fbea844d43315d03796b5204912a383c7bbc
Pedro Junho Silveira <pjunhosilveira@gmail.com>
2022-06-22 10:08:12 -0300

    Update multmatriz.c

--- multmatriz.c
+++ multmatriz.c
@@ -8,14 +8,12 @@ int main() {
   int mat2[3][3];
   int mult[3][3];
   for (i = 0; i < 3; i++)
-    for (j = 0; j < 3; j++) {
+    for (j = 0; j < 3; j++) 
       scanf("%d", &mat1[i][j]);
-    }
   printf("\n");
   for (i = 0; i < 3; i++)
-    for (j = 0; j < 3; j++) {
+    for (j = 0; j < 3; j++) 
       scanf("%d", &mat2[i][j]);
-    }
   for (i = 0; i < 3; i++)
     for (j = 0; j < 3; j++) {
       sum= 0;
